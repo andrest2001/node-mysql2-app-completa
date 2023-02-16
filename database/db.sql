02884d8403c0f1bc589cee9409fa492f3ba2dd1a
Andrés Toledano <andres13072001@gmail.com>
2023-02-16 10:11:21 +0100

    Encriptado de contraseña

--- database/db.sql
+++ database/db.sql
@@ -5,7 +5,7 @@ USE database_links;
 CREATE TABLE users(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL,
-    password VARCHAR(30) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     email VARCHAR(70) NOT NULL,
     fullname VARCHAR(100) NOT NULL
 );
